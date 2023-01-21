502db9419f3db650abe78a74aa9441982dc80cf9
andinianst93 <andinianst93@gmail.com>
2023-01-21 22:28:24 +0700

    asda

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,22 +3,21 @@ import Image from 'next/image'
 import Link from 'next/link'
 const Hero = () => {
   return (
-    <section>
-      <div className='grid px-4 py-8 md:px-16 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 bg-slate-900 max-w-full'>
-        <div className='mr-auto place-self-center lg:col-span-7 '>
-          <h1 className='max-w-2xl text-white mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl'>
-            Lorem ipsum dolor sit amet
-          </h1>
-          <p className='max-w-2xl mb-6 font-light text-gray-300 lg:mb-8 md:text-lg lg:text-xl'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem
-            voluptas harum placeat quam in quos minima. Veritatis et id
-            deleniti!
-          </p>
+    <section className='bg-slate-900 max-w-full md:flex md:justify-center md:items-center'>
+      <div className='px-4 py-8 md:px-16 lg:py-16'>
+        <h1 className='max-w-2xl text-white mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl md:text-center'>
+          Let’s Collab!
+        </h1>
+        <p className='max-w-2xl mb-6 font-light text-gray-300 lg:mb-8 md:text-lg md:text-center'>
+          Kamu dapat mengirimkan artikel berupa tips, studi kasus, atau opini
+          seputar dunia digital marketing di sini. Ayo, daftar sekarang juga!
+        </p>
+        <div className='md:flex md:items-center md:justify-center'>
           <Link
             href='/contact'
-            className='inline-flex text-white items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center rounded-lg bg-blue-700 hover:bg-blue-900 focus:ring-4 focus:ring-blue-300'
+            className='text-white inline-flex justify-center items-center px-5 py-3 mr-3 text-base font-medium text-center rounded-lg bg-blue-700 hover:bg-blue-900 focus:ring-4 focus:ring-blue-300'
           >
-            Get started
+            Mulai Sekarang
             <svg
               className='w-5 h-5 ml-2 -mr-1'
               fill='currentColor'
@@ -33,7 +32,6 @@ const Hero = () => {
             </svg>
           </Link>
         </div>
-        <div className='hidden lg:mt-0 lg:col-span-5 lg:flex'></div>
       </div>
     </section>
   )
